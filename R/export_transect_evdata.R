250a4743ad2bd85818c35646756133321ff7b957
Mark <mdufour@usgs.gov>
2022-01-28 08:00:08 -0500

    Added "(1)" to #Export Cruise Track code

--- R/export_transect_evdata.R
+++ R/export_transect_evdata.R
@@ -33,7 +33,7 @@ export_transect_evdata <- function(prjdir, transectname, horizbin) {
   EVFile<-EVAppObj$OpenFile(EVFile2Open)
 
   # Export Cruise Track
-  GPS<-EVFile[["Variables"]]$FindByName("Fileset1: position GPS fixes")
+  GPS<-EVFile[["Variables"]]$FindByName("Fileset1: position GPS fixes (1)")
   if(GPS$ExportData(file.path(transect_dir, 'gps.csv'))) {
     usethis::ui_done("GPS track exported")
   } else {
