a82cb759c89254d02d463f0dd343dc8b6fd472ba
J. Holden <jeremy.holden@ontario.ca>
2022-02-28 10:33:19 -0500

    updates to Analysis_Import_EVcsv
    Added error checks and usethis messages for directory structure and file naming convention

--- R/Analysis_Import_EVcsv.R
+++ R/Analysis_Import_EVcsv.R
@@ -9,6 +9,12 @@
 #' hacdat <- Analysis_Import_EVcsv()
 #'  }
 Analysis_Import_EVcsv <- function(){
+  # check "3_Ping_Data" directory exists
+  if(!dir.exists("3_Ping_Data")){
+    usethis::ui_oops("Can't find '/3_Ping_Data' directory.")
+    stop("Directory format not valid.")
+  }
+
   # read all files
   allfiles<-dir("3_Ping_Data", recursive = T, full.names = T)
   # get only the intg.csv
@@ -18,10 +24,21 @@ Analysis_Import_EVcsv <- function(){
   # histo.csv
   histo<-grep(allfiles, pattern = "histo\\.csv$", value = T)
 
+  # check that intg, ts and histo has length >0
+  if(length(intg)==0){
+    usethis::ui_oops("Expecting to find intg.csv files and found none.")
+  }
+  if(length(ts)==0){
+    usethis::ui_oops("Expecting to find ts.csv files and found none.")
+  }
+  if(length(histo)==0){
+    usethis::ui_oops("Expecting to find histo.csv files and found none.")
+  }
+
   # read in all csv files and combine as single df
-  intg<-bind_rows(lapply(intg, readr::read_csv))
-  ts<-bind_rows(lapply(ts, readr::read_csv))
-  histo<-bind_rows(lapply(histo, readr::read_csv))
+  intg<-dplyr::bind_rows(lapply(intg, readr::read_csv))
+  ts<-dplyr::bind_rows(lapply(ts, readr::read_csv))
+  histo<-dplyr::bind_rows(lapply(histo, readr::read_csv))
 
   # store and return as a list
   hacdat <- list(intg, ts, histo)
