e9f84ba21f915b3cab630778db4b64ba4da017b3
Mark <mdufour@usgs.gov>
2022-09-14 13:55:18 -0400

    Aligned param names and descriptions tranname -> transectname BIOSONIC -> BIOSONICS prjdir -> projecthome

--- R/set_up_transect.R
+++ R/set_up_transect.R
@@ -6,9 +6,9 @@
 #' @param projecthome A file path to the project directory. Generally, when working in an
 #' RStudio project environment you can populate this field using `getwd()`
 #' @param sonartype The function uses a text pattern matching to find acoustic data files.
-#' Function expects an input of *BIOSONIC* or *SIMRAD* to detect \*.dt4 or \*.raw
+#' Function expects an input of *BIOSONICS* or *SIMRAD* to detect \*.dt4 or \*.raw
 #' file types.
-#' @param tranname Transect name that matches a valid directory in *3_Ping_Data/* directory
+#' @param transectname Transect name that matches a valid directory in *3_Ping_Data/* directory
 #'
 #' @description This function calls RDCOMClient to establish a COM application for Echoview.
 #' This function is primarily a wrapper for functions found in EchoviewR
@@ -30,29 +30,29 @@
 #'file.exists(evtemplate)
 #'
 #'set_up_transect(evtemplate, projecthome = getwd(),
-#'                sonartype = "SIMRAD", tranname = "ERIE")
+#'                sonartype = "SIMRAD", transectname = "ERIE")
 #'set_up_transect(evtemplate, projecthome = getwd(),
-#'              sonartype = "BIOSONIC", tranname = "COB")
+#'              sonartype = "BIOSONICS", transectname = "COB")
 #'}
 #'
 
-set_up_transect<-function (template, projecthome, sonartype, tranname) {
+set_up_transect<-function (template, projecthome, sonartype, transectname) {
   if(!file.exists(template)) {
     usethis::ui_oops("It appears as though your template doesn't exist")
   }
 
   sonartype <- toupper(sonartype)
-  if(!(sonartype %in% c("BIOSONIC", "SIMRAD"))){
-    usethis::ui_oops("Not a valid sounder type. Please specify BIOSONIC or SIMRAD")
+  if(!(sonartype %in% c("BIOSONICS", "SIMRAD"))){
+    usethis::ui_oops("Not a valid sounder type. Please specify BIOSONICS or SIMRAD")
 
   }
 
   stopifnot(exprs = {
     file.exists(template)
-    sonartype %in% c("BIOSONIC", "SIMRAD")
+    sonartype %in% c("BIOSONICS", "SIMRAD")
     })
 
-  sonarfilepattern <- ifelse(sonartype == "BIOSONIC", "dt4$", "raw$")
+  sonarfilepattern <- ifelse(sonartype == "BIOSONICS", "dt4$", "raw$")
 
   requireNamespace("RDCOMClient")
   library(RDCOMClient)
@@ -62,7 +62,7 @@ set_up_transect<-function (template, projecthome, sonartype, tranname) {
   EvFile <- EvApp$NewFile(template)
 
   # get then load dt4 files
-  dt4_dir<-file.path(projecthome, "3_Ping_Data", tranname)
+  dt4_dir<-file.path(projecthome, "3_Ping_Data", transectname)
   files_all<-dir(dt4_dir, full.names = T)
   dt4<-grep(files_all, pattern = sonarfilepattern, value = T)
 
@@ -70,13 +70,13 @@ set_up_transect<-function (template, projecthome, sonartype, tranname) {
     EvFile$Close()
     EvApp$Quit()
     usethis::ui_oops("No valid acoustic formats found")
-    stop(paste('no dt4 files found in', tranname, sep = ' '))
+    stop(paste('no dt4 files found in', transectname, sep = ' '))
   }
 
   EchoviewR::EVAddRawData(EvFile, 'Fileset1', dt4)
 
   # Save
-  evfilename<- paste(tranname, ".EV", sep = '')
+  evfilename<- paste(transectname, ".EV", sep = '')
   #message(evfilename)
   evfilefull<-file.path(dt4_dir, evfilename)
   #message(evfilefull)
@@ -94,7 +94,7 @@ set_up_transect<-function (template, projecthome, sonartype, tranname) {
 
   # Export processed Sv ('ExportSv') as .png image
   ExportSvVar = EvFile[['Variables']]$FindByName('ExportSv')                                ## find and define variable to be exported
-  image.file.name = paste0(basename(tranname),'.png')                                       ## define image file name
+  image.file.name = paste0(basename(transectname),'.png')                                       ## define image file name
   ExportSvVar$ExportEchogramToImage(file.path(dt4_dir,image.file.name,fsep='\\'),10000,-1,-1) ## export to dt4_dir folder
 
 
@@ -104,6 +104,6 @@ set_up_transect<-function (template, projecthome, sonartype, tranname) {
   # Close and quit
   EvFile$Close()
   EvApp$Quit()
-  usethis::ui_done(paste(tranname, 'has been completed'))
+  usethis::ui_done(paste(transectname, 'has been completed'))
   # end
 }
