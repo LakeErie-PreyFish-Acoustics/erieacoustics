89762c10fad8a581420b82eada058a09dcb32f4c
J. Holden <jeremy.holden@ontario.ca>
2022-09-16 17:18:47 -0400

    adds effort allocaton file structure.
    See issue #35. The current effort allocation table is incomplete but this commit provides the structure to modify when the allocation table exists.

---
+++ data-raw/Effort_Allocation.R
@@ -0,0 +1,4 @@
+## code to prepare `Effort_Allocation` dataset goes here
+library(readr)
+Effort_Allocation <- read_csv("data-raw/eb_effort_allocation.csv")
+usethis::use_data(Effort_Allocation, overwrite = TRUE)
