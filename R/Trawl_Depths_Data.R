0292b5332e4b7f7aa83842d4b6c8ea1d7bec0bad
Mark <mdufour@usgs.gov>
2022-11-15 13:39:40 -0500

    Added Trawl_Depths and Vessel_Log data to this branch.Updated Effort_Allocation on this branch.

---
+++ R/Trawl_Depths_Data.R
@@ -0,0 +1,11 @@
+#' Trawl Depths
+#' @name Trawl_Depths
+#' @description This table contains the target trawl depths for each site by strata.
+#' @return dataframe
+#'
+#' @examples
+#' data("Trawl_Depths")
+#' head(Trawl_Depths)
+
+data("Trawl_Depths")
+Trawl_Depths
---
+++ R/Vessel_Log_Data.R
@@ -0,0 +1,11 @@
+#' Vessel Log
+#' @name Vessel_Log
+#' @description This table contains a vessel log for documentation during data collection.
+#' @return dataframe
+#'
+#' @examples
+#' data("Vessel_Log")
+#' head(Vessel_Log)
+
+data("Vessel_Log")
+Vessel_Log
