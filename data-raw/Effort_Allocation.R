8578550656612e8aa00e30e77163a0a8f3b7787c
Mark <mdufour@usgs.gov>
2022-11-15 12:54:09 -0500

    Updated Effort_Allocation

--- data-raw/Effort_Allocation.R
+++ data-raw/Effort_Allocation.R
@@ -1,4 +1,5 @@
 ## code to prepare `Effort_Allocation` dataset goes here
 library(readr)
-Effort_Allocation <- read_csv("data-raw/effort_allocation.csv")
+
+Effort_Allocation <- read_csv("data-raw/Effort_Allocation.csv")
 usethis::use_data(Effort_Allocation, overwrite = TRUE)
